859479e227f2e1e0f4ca621bef1d7fc4919cb6d6
Takashi Kitajima <inc@2inc.org>
2017-09-13 10:52:31 +0900

    Added section component

---
+++ src/component/section.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @package inc2734/wp-awesome-components
+ * @author inc2734
+ * @license GPL-2.0+
+ */
+
+return [
+	'section' => [
+		'label' => __( 'Section', 'inc2734-wp-awesome-components' ),
+		'html'  => sprintf(
+			'<div class="wpac-section mceNonEditable">
+				<div class="wpac-section__container">
+					<h2 class="wpac-section__title mceEditable">%1$s</h2>
+					<div class="wpac-section__body mceEditable">
+						%1$s
+					</div>
+				</div>
+			</div>',
+			esc_html__( 'Section', 'inc2734-wp-awesome-components' )
+		),
+	],
+
+	'section--inverse' => [
+		'label' => __( 'Section (Inverse)', 'inc2734-wp-awesome-components' ),
+		'html'  => sprintf(
+			'<div class="wpac-section wpac-section--inverse mceNonEditable">
+				<div class="wpac-section__container">
+					<h2 class="wpac-section__title mceEditable">%1$s</h2>
+					<div class="wpac-section__body mceEditable">
+						%1$s
+					</div>
+				</div>
+			</div>',
+			esc_html__( 'Section', 'inc2734-wp-awesome-components' )
+		),
+	],
+];
